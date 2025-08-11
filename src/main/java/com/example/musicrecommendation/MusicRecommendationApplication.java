2b62077611f82d2dc932acfb67454a703a602c23
hyeonker <root7dll@gmail.com>
2025-08-11 21:31:21 +0900

    Changes

---
+++ src/main/java/com/example/musicrecommendation/MusicRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRecommendationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicRecommendationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/musicrecommendation/MusicRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicrecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicRecommendationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
